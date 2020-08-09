ca7ba6070c684621688dae0298529e784de41c1b
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-09 15:42:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   array = [1, 2, 3]
   new_array = []
   array.length.times do |index|
-    new_array.push(array**[index])
+    new_array.push*(array[index])
   end
   new_array
 end

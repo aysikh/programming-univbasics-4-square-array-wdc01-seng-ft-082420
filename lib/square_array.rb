d8a3f1f50640c31c3f5b1645afe7ea0785b84b8f
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-09 15:55:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,3 +6,12 @@ def square_array(numbers)
   end
   new_array
 end
+
+def square_array(new_numbers)
+  new_numbers = [9, 10, 16, 25]
+  new_array = []
+  new_numbers.length.times do |index|
+    new_array.push(new_numbers[index] ** 2)
+  end
+  new_array
+end
